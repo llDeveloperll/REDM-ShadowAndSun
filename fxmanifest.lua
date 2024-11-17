7d007f8e962ba56fabed529094b173c13caa1124
Vilaum <louishasfield@gmail.com>
2024-11-17 00:48:29 -0300

    Updated fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'Vilão.dev'
+description '**Sun and Shadow Detection Script** by Vilão.dev.'
+version '1.0.0'
+lua54 'yes'
+
+framework "standalone"
+
+client_scripts {
+    'main.lua'
+}
